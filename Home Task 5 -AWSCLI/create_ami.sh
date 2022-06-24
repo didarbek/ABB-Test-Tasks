79fb9524284a0b95d70dfbb7066d7da8a483aea1
Didarbek Nuraliyev <didarbeknuraliyev@Didarbeks-MacBook-Air.local>
2022-06-24 19:53:54 +0400

    added image_id

--- Home Task 5 -AWSCLI/create_ami.sh
+++ Home Task 5 -AWSCLI/create_ami.sh
@@ -2,10 +2,13 @@
 
 
 # using create-image to create out image
-aws ec2 create-image \
+create_image = aws ec2 create-image \
     --instance-id i-1234567890abcdef0 \
     --name "My server" \
     --description "An AMI for my server"
 
+# getting "image_id" from json output
+image_id = $(create_image | jq '.ImageId')
+
 # using wait image-available to wait till image will be available
-aws ec2 wait image-available --image-ids ${image-id} --filter "Name=state,Values=available"
+aws ec2 wait image-available --image-ids ${image_id} --filter "Name=state,Values=available"
